7ed89e0ff860d1497e624210f3236bd7067cdc96
edong <edong1111@hotmail.com>
2022-07-08 22:18:17 +0800

    去掉Migration

--- aspnetcore/security/authentication/add-user-data/samples/6.x/SampleApp/Program.cs
+++ aspnetcore/security/authentication/add-user-data/samples/6.x/SampleApp/Program.cs
@@ -1,13 +1,46 @@
+using System.IdentityModel.Tokens.Jwt;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using WebApp1.Areas.Identity.Data;
 using WebApp1.Data;
 var builder = WebApplication.CreateBuilder(args);
-var connectionString = builder.Configuration.GetConnectionString("WebApp1ContextConnection");builder.Services.AddDbContext<WebApp1Context>(options =>
-    options.UseSqlServer(connectionString));builder.Services.AddDefaultIdentity<WebApp1User>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<WebApp1Context>();
+var connectionString = builder.Configuration.GetConnectionString("WebApp1ContextConnection");
+builder.Services.AddDbContext<WebApp1Context>(options => options.UseSqlServer(connectionString));
+
+//builder.Services.AddDefaultIdentity<WebApp1User>(options => options.SignIn.RequireConfirmedAccount = true)
+//    .AddEntityFrameworkStores<WebApp1Context>();
+JwtSecurityTokenHandler.DefaultMapInboundClaims = false;
+JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+builder.Services.AddIdentity<WebApp1User, IdentityRole>()
+                .AddEntityFrameworkStores<WebApp1Context>();
+//注册时的配置选项 去掉密码复杂性要求
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    // Password settings.
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    //options.Password.RequiredLength = 6;
+    options.Password.RequiredUniqueChars = 1;
+
+    // Lockout settings.
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.AllowedForNewUsers = true;
+
+    // User settings.
+    options.User.AllowedUserNameCharacters = null;
+    //"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+    options.User.RequireUniqueEmail = false;
+});
 
 builder.Services.AddRazorPages();
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("RequireAdministratorRole",
+         policy => policy.RequireRole("adminss"));
+});
 
 var app = builder.Build();
 
@@ -24,6 +57,15 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapRazorPages();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+    endpoints.MapGet("/Process", async context =>
+    {
+        await context.Response.WriteAsync("Worker Process Name : " +
+            System.Diagnostics.Process.GetCurrentProcess().ProcessName);
+    });
+});
 
+app.MapRazorPages();
 app.Run();
