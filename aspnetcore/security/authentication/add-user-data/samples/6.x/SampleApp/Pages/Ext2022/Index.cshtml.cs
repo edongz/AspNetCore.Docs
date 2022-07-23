e4ae3ec5b82af738aaf2989627f14772af760390
edong <edong1111@hotmail.com>
2022-07-23 15:51:22 +0800

    bundleconfig.json

---
+++ aspnetcore/security/authentication/add-user-data/samples/6.x/SampleApp/Pages/Ext2022/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp1.Pages.Ext2022
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
